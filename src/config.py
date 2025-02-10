8363b3d26a82e448f82629a2a461ea0282c5d031
pabgarcialopez <pabgar30@ucm.es>
2025-02-10 14:33:38 +0100

    logs changes

---
+++ src/config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Experiment parameters
+NUM_EXPERIMENTS = 10      # Number of experiments per transition probability
+MIN_PROB = 0.1            # Minimum transition probability
+MAX_PROB = 1.0            # Maximum transition probability
+NUM_PROBS = 10            # Number of probability values to test
+
+# Plotting options
+GENERATE_PLOTS = True     # Set to False if you do not want to generate plots
